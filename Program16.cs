3738799eea54db6ceae5eccc961e4f19eb00620e
REKHA20003005 <121917644+REKHA20003005@users.noreply.github.com>
2023-01-04 12:59:03 +0530

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+ 
+
+class CompareGenericClass < T > {  
+    public bool Compare(T x, T y) {  
+        if (x.Equals(y)) 
+       return true;  
+        else 
+       return false;  
+    }  
+}
+
+ 
+
+class Program {  
+    static void Main(string[] args) {  
+        CompareGenericClass < string > Ocompare = new CompareGenericClass < string > ();  
+        bool stringResult = Ocompare.Compare("Hey There", "Hey There");  
+        Console.WriteLine("Generic string comapre result:" + stringResult);  
+
+ 
+
+        CompareGenericClass < int > oIntcompare = new CompareGenericClass < int > ();  
+        bool integerresult = oIntcompare.Compare(15, 16);  
+        Console.WriteLine("Generic int comapre result:" + integerresult);  
+    }  
+}
